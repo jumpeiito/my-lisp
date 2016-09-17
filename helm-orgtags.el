e08757dceebc3d77008551d7b80cf4d8bfc56254
jumpei <junnpit@gmail.com>
2016-09-18 01:57:52 +0900

    Added helm-orgtags.el

---
+++ helm-orgtags.el
@@ -0,0 +1,24 @@
+(load "f:/.tags")
+
+(defun helm-orgtags-insert (slist)
+  (cl-flet ((concat-colon (a b) (concat a ":" b)))
+    (insert (concat ":"
+		    (cl-reduce #'concat-colon slist)
+		    ":"))))
+
+
+(defvar helm-orgtags-source
+  (helm-build-sync-source "OrgNews Tags"
+    :candidates org-newspaper-tags-list
+    :action     (lambda (e) (helm-orgtags-insert (helm-marked-candidates)))
+    :migemo     t))
+
+(defun helm-orgtags ()
+  (interactive)
+  (helm :sources '(helm-orgtags-source)
+	:buffer "*helm orgtags*"))
+
+(helm-migemize-command helm-orgtags)
+
+
+
